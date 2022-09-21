5399e928b3b073d2b7403b929dbce19b49e5719c
Jean Carlos <jcpds14@gmail.com>
2022-09-21 17:27:26 -0300

    add object of create data account

--- Banco/Banco/Program.cs
+++ Banco/Banco/Program.cs
@@ -6,7 +6,24 @@ namespace Banco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ContaCorrente conta1 = new ContaCorrente();
+
+            Console.Write("Digite o nome do titular: ");
+            conta1.titular = Console.ReadLine();
+
+            Console.Write("Digite a agência (Ex: xxxx-x): ");
+            conta1.agencia = Console.ReadLine();
+            Console.Write("Digite o número da conta (Ex: xxxx-x): ");
+            conta1.numero = Console.ReadLine();
+            Console.Write("Saldo inicial: ");
+            conta1.saldo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nEstas são as informações da conta:\n");
+
+            Console.WriteLine($"Titular da conta: {conta1.titular}");
+            Console.WriteLine($"Agência: {conta1.agencia}");
+            Console.WriteLine($"Número da conta: {conta1.numero}");
+            Console.WriteLine($"Saldo Total: {conta1.saldo}");
         }
     }
 }
